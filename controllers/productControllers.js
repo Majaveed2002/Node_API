9b6481b961f575c632700f0ba8cdf9b04e08af68
Majaveed2002 <134384853+Majaveed2002@users.noreply.github.com>
2023-12-21 14:09:45 +0530

    finished the Node Api App

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,66 +1,70 @@
 const express = require("express")
 const Product = require("../models/productModules")
+const asyncHandler = require('express-async-handler')
 
-const getProducts =  async(req,res)=> {
+const getProducts =  asyncHandler(async(req,res)=> {
   try {
     const products = await Product.find({})
     res.status(200).json(products)
     
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({message:error.message})
+    res.status(500)
+    throw new Error(error.message)
   }
-}
+})
 
-const getProduct = async (req,res) => {
+const getProduct = asyncHandler( async (req,res) => {
   try {
     const {id} = req.params
     const product = await Product.findById(id)
     res.status(200).json(product)
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({message:error.message})
+    res.status(500)
+    throw new Error(error.message)
   }
-}
+})
 
-const updatedProduct = async (req,res) => {
+const updatedProduct = asyncHandler(async (req,res) => {
   try {
     const {id} = req.params
     const product = await Product.findByIdAndUpdate(id,req.body)
     if(!product){
-      return res.status(404).json({message: `Cannot Find Any Product With ID ${id}`})
+      res.status(404)
+    throw new Error(`Cannot Find Any Product With ID ${id}`)
     }
     const updatedProduct = await Product.findById(id)
     res.status(200).json(updatedProduct)
-  } catch (error) {
-    console.log(error.message)
-    res.status(500).json({message:error.message})
   }
-}
+  catch (error) {
+    res.status(500)
+    throw new Error(error.message)
+  }
+})
 
-const addProduct = async(req,res)=> {
+const addProduct = asyncHandler(async(req,res)=> {
   try {
     const product = await Product.create(req.body)
     res.status(200).json(product)
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({message:error.message})
+    res.status(500)
+    throw new Error(error.message)
   }
-}
+})
 
-const deleteProduct = async (req,res) => {
+const deleteProduct = asyncHandler(async (req,res) => {
   try {
     const {id} = req.params
     const product = await Product.findByIdAndDelete(id)
     if(!product){
-      return res.status(404).json({message: `Cannot Find Any Product With ID ${id}`})
+      res.status(404)
+    throw new Error(`Cannot Find Any Product With ID ${id}`)
     }
     res.status(200).json({message: `The ${product.name} Product was Deleted From Database`})
   } catch (error) {
-    console.log(error.message)
-    res.status(500).json({message:error.message})
+    res.status(500)
+    throw new Error(error.message)
   }
-}
+})
 
 module.exports = {
   getProducts,
